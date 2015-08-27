a79e51e0924b4160b6edebd4511121b4267da35b
Mayank Jain <mayank.jain@tradus.com>
2015-08-27 20:47:27 +0530

    Testing

--- c2/attributes/default.rb
+++ c2/attributes/default.rb
@@ -1,1 +1,2 @@
 default['c2_var'] = 'this is c2_var'
+default["file_content"] = "FOOBAR"
--- c2/recipes/default.rb
+++ c2/recipes/default.rb
@@ -2,3 +2,15 @@ log("C2 default")
 log(node["c2_var"])
 
 include_recipe("c1")
+
+system("whoami")
+system("pwd")
+puts "#{__FILE__}"
+puts "#{File.dirname(__FILE__)}"
+
+file '/foo' do
+  content node["file_content"]
+  mode '0755'
+  owner 'root'
+  group 'root'
+end
