bc23ae90b3a179e0eea372c125f14d910d49c1dc
Mayank Jain <mayank.jain@tradus.com>
2015-08-28 01:40:09 +0530

    Added mongoid.yml

--- my_mongo/recipes/default.rb
+++ my_mongo/recipes/default.rb
@@ -2,3 +2,8 @@ log("Starting MyMongo Recipe")
 
 include_recipe("mongodb::mongodb_org_repo")
 include_recipe("mongodb")
+
+log("Going to generate mongoid.yml file")
+template "/mongoid.yml" do
+  source "mongoid.yml.erb"
+end
